79ed3a6ec74c29901cccdd24b494c9e304796bb5
Ivan Polkanov <polkanovclassicpro@gmail.com>
2023-06-29 15:56:35 -0700

    Init commit

---
+++ LocalStorage/LocalStorage.cs
@@ -0,0 +1,57 @@
+﻿namespace LocalStorage;
+
+public class LocalStorage
+{
+    public static LocalStorage Instance { get; private set; }
+
+    private Dictionary<string, object> _data = new Dictionary<string, object>();
+    private string _saveFilePath;
+
+    public LocalStorage(string filePath)
+    {
+        _saveFilePath = filePath;
+
+        var dirName = Path.GetDirectoryName(filePath);
+        if (!Directory.Exists(dirName))
+            Directory.CreateDirectory(dirName);
+
+        if (!File.Exists(filePath))
+            _data = new Dictionary<string, object>();
+        else
+        {
+            try
+            {
+                _data = BinarySerializer.ReadFromBinaryFile<Dictionary<string, object>>(filePath);
+            }
+            catch (Exception ex)
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        Instance = this;
+    }
+
+    public object Get(string key)
+    {
+        if (_data.TryGetValue(key, out var result))
+            return result;
+        else
+            return null;
+    }
+
+    public void Set(string key, object value)
+    {
+        _data[key] = value;
+    }
+
+    public bool ContainsKey(string key)
+    {
+        return _data.ContainsKey(key);
+    }
+
+    public void Save()
+    {
+        BinarySerializer.WriteToBinaryFile(_saveFilePath, _data);
+    }
+}

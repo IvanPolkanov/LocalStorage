48f053988bcb30c6d6188d89e381d760d3b03f42
Ivan Polkanov <polkanovclassicpro@gmail.com>
2023-07-01 08:43:40 -0700

    Add examples

---
+++ JustLocalStorageExamples/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace JustLocalStorageExamples;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Init();
+
+        var intList = GenerateData();
+
+        SaveData(intList);
+    }
+
+    private static List<int> GenerateData()
+    {
+        var intList = new List<int>();
+        var rnd = new Random();
+        for (int i = 0; i < 100; i++)
+        {
+            intList.Add(rnd.Next());
+        }
+
+        return intList;
+    }
+
+    private static void Init()
+    {
+        var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        var fileName = Path.Combine(appDataPath, "JustLocalStorageExamples\\LocalStorage.bin");
+
+        new LocalStorage.LocalStorage(fileName);
+    }
+
+    private static void SaveData(object toSave)
+    {
+        var storage = LocalStorage.LocalStorage.Instance;
+
+        storage.Set("MyBestData", toSave);
+
+        storage.Save();
+    }
+}
